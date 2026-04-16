a57118fc138a3085bcbe41171d7c4cd1f69a602a
MarioLuigi0404 <58757052+MarioLuigi0404@users.noreply.github.com>
2026-04-15 23:42:47 -0600

    dev environment proxy setup

--- easyembed-ui/vite.config.js
+++ easyembed-ui/vite.config.js
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      "/upload": "http://127.0.0.1:8000",
+      "/status": "http://127.0.0.1:8000",
+      "/files": "http://127.0.0.1:8000",
+      "/media": "http://127.0.0.1:8000"
+    }
+  }
 })
